472c213f08a366c50787d42ed70865b9c2e83fc1
Sageeth Himachala <sageethhimachala@gmail.com>
2026-08-20 10:23:28 +0530

    Commented featured component

--- my-react-app/src/pages/home/Home.jsx
+++ my-react-app/src/pages/home/Home.jsx
@@ -1,28 +1,28 @@
-import './home.css'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
-import Featured from '../../components/featured/Featured'
-import PropertyList from '../../components/propertyList/PropertyList'
-import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
-import MailList from '../../components/mailList/MailList'
-import Footer from '../../components/footer/Footer'
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
 
 function Home() {
   return (
     <div>
-      <Navbar/>
-      <Header/>
+      <Navbar />
+      <Header />
       <div className="homeContainer">
-        <Featured />
+        {/* <Featured /> */}
         <h1 className="homeTitle">Browse by property type</h1>
-        <PropertyList/>
+        <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
-        <FeaturedProperties/>
-        <MailList/>
-        <Footer/>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
